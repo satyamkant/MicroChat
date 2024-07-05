0b61a599198510e59f3de2d0b61312cc332c47bf
satyamkant <satyamkant2805@gmail.com>
2024-07-05 18:27:59 +0530

    added first controller method

--- Backend/userservice/src/main/java/com/microchat/userservice/controller/UserController.java
+++ Backend/userservice/src/main/java/com/microchat/userservice/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.microchat.userservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class UserController {
 
+    @PutMapping("/registration")
+    ResponseEntity<String> registerUser(){
+
+        return new ResponseEntity<>("Registered successfully!", HttpStatus.OK);
+    }
 }
